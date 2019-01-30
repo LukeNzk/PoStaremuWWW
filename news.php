20446042b1f58de17fb6d9b7191002733ac0f6bf
Elejdor <elejdor@gmail.com>
2019-01-30 00:32:25 +0000

    Added news content

---
+++ news.php
@@ -0,0 +1,30 @@
+<div id="tiles">
+<div class="tile" 
+day="29" month="XII" time="17:00" venue="Poleski Ośrodek Sztuki"
+title="Kolędniczy Potańc Po Staremu" 
+url="https://www.facebook.com/events/199109267695571/" 
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/47441220_2269027789838005_8568996476108144640_o.jpg?_nc_cat=104&_nc_eui2=AeHMqhgUJDy1cAU2LbMQgt876YMLyEJgY1mhu_eqmBBT9l5za1XZPY9SX7Ff-HKC2OggZFxFFPBuIihQbzxtqTQXqaNjbadFPonOLftKuB0gOw&_nc_ht=scontent-frt3-1.xx&oh=26247e89f898ee253729b46e3fad83b1&oe=5C8D5CE7"
+desc="Nauka tradycyjnych kolęd, warsztaty tańca dla początkujących. Zagrają Janusz Prusinowski, kapela Przodki oraz muzycy łódzkiej sceny tradycji.">
+</div>
+
+<div class="tile" 
+day="31" month="XII" time="20:00" venue="Poleski Ośrodek Sztuki"
+title="Sylwester u Pana Gara" 
+url="https://www.facebook.com/events/1087382928095875/" 
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/47318117_2399193920100223_7079247031805411328_o.jpg?_nc_cat=100&_nc_eui2=AeGWQZS8oWJH5Wp4xfhdmzhPir_Lbxi7Kesk_-GK-NlWlu7t9kfl2WPJ3nnuooKDbnJZPssk01MFv2YyhFo1Vb03J0UJGaQfElqh9yf7-Fn2pA&_nc_ht=scontent-frt3-1.xx&oh=a5a528f0b68dfb24b99e69f9255839b8&oe=5C93D1EC"
+desc="Impreza sylwestrowa przy muzyce tradycyjnej w Poznańu.<br/>
+Zagrają:<br/>
+* Przodki (Poznań)<br/>
+* Diabubu (Łódź)<br/>
+* Napięcie (Łódź)">
+</div>
+
+<div class="tile" 
+day="6" month="I" time="9:00" venue="Łódź"
+title="Łódzkie Pogotowie Kolędnicze" 
+url="https://www.facebook.com/events/2165300870398818/" 
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/47156260_2266883533385764_2985220571022753792_n.jpg?_nc_cat=101&_nc_eui2=AeHU9aAgQDwgz6VK5dECuVM1BxdzUOJshLOXZ7LWmkNUWRQSTYDpfF-NVllMSOHF_KqqrDZJU1ePD_K2h2UHuAbpb1_vPG4uG6JomR_uP0DRRA&_nc_ht=scontent-frt3-1.xx&oh=b82e2c190b7ea9d2735d9c7d49072d2c&oe=5CD1F9B9"
+desc="Zaproś do siebie pogotowie kolędnicze! Wystarczy wypełnić <a href='https://goo.gl/forms/GJ5ZIPWjfG8B7OCz1'>formularz</a>, a przyjedziemy do Ciebie grać i śpiewać kolędy.">
+</div>
+</div>
+
