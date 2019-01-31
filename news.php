ebfcc3baf12ea05ef5c7cd2423893e96aafec815
elejdor <elejdor@gmail.com>
2019-01-31 23:38:09 +0100

    Kożuch // Bonanza // Spotkania mazurkowe

--- news.php
+++ news.php
@@ -1,30 +1,26 @@
 <div id="tiles">
 <div class="tile" 
-day="29" month="XII" time="17:00" venue="Poleski Ośrodek Sztuki"
-title="Kolędniczy Potańc Po Staremu" 
-url="https://www.facebook.com/events/199109267695571/" 
-img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/47441220_2269027789838005_8568996476108144640_o.jpg?_nc_cat=104&_nc_eui2=AeHMqhgUJDy1cAU2LbMQgt876YMLyEJgY1mhu_eqmBBT9l5za1XZPY9SX7Ff-HKC2OggZFxFFPBuIihQbzxtqTQXqaNjbadFPonOLftKuB0gOw&_nc_ht=scontent-frt3-1.xx&oh=26247e89f898ee253729b46e3fad83b1&oe=5C8D5CE7"
-desc="Nauka tradycyjnych kolęd, warsztaty tańca dla początkujących. Zagrają Janusz Prusinowski, kapela Przodki oraz muzycy łódzkiej sceny tradycji.">
+day="3" month="II" time="16:30" venue="Poczekalnia / Więckowskiego"
+title="Karnawałowy Potańc Po Staremu // Kożuch" 
+url="https://www.facebook.com/events/221866625391755/" 
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/49773442_2354821757925274_6046371520979140608_o.jpg?_nc_cat=100&_nc_ht=scontent-frt3-1.xx&oh=4ef0397934dbedd9356b0108e2a69417&oe=5CFC86B9"
+desc="Impreza folkowa z muzykami znakomitej kapeli Kożuch. Przed imprezą warsztaty podstaw tańca oraz gry na bębenku obręczowym.">
 </div>
 
 <div class="tile" 
-day="31" month="XII" time="20:00" venue="Poleski Ośrodek Sztuki"
-title="Sylwester u Pana Gara" 
-url="https://www.facebook.com/events/1087382928095875/" 
-img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/47318117_2399193920100223_7079247031805411328_o.jpg?_nc_cat=100&_nc_eui2=AeGWQZS8oWJH5Wp4xfhdmzhPir_Lbxi7Kesk_-GK-NlWlu7t9kfl2WPJ3nnuooKDbnJZPssk01MFv2YyhFo1Vb03J0UJGaQfElqh9yf7-Fn2pA&_nc_ht=scontent-frt3-1.xx&oh=a5a528f0b68dfb24b99e69f9255839b8&oe=5C93D1EC"
-desc="Impreza sylwestrowa przy muzyce tradycyjnej w Poznańu.<br/>
-Zagrają:<br/>
-* Przodki (Poznań)<br/>
-* Diabubu (Łódź)<br/>
-* Napięcie (Łódź)">
+day="12" month="II" time="18:00" venue="Łódzki Dom Kultury"
+title="Łódzkie Spotkania Mazurkowe" 
+url="https://www.facebook.com/events/630671224032274/" 
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
+desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
 </div>
 
 <div class="tile" 
-day="6" month="I" time="9:00" venue="Łódź"
-title="Łódzkie Pogotowie Kolędnicze" 
+day="15" month="II" time="21:00" venue="Łódź"
+title="Orkiestra Taneczna Bonanza" 
 url="https://www.facebook.com/events/2165300870398818/" 
-img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/47156260_2266883533385764_2985220571022753792_n.jpg?_nc_cat=101&_nc_eui2=AeHU9aAgQDwgz6VK5dECuVM1BxdzUOJshLOXZ7LWmkNUWRQSTYDpfF-NVllMSOHF_KqqrDZJU1ePD_K2h2UHuAbpb1_vPG4uG6JomR_uP0DRRA&_nc_ht=scontent-frt3-1.xx&oh=b82e2c190b7ea9d2735d9c7d49072d2c&oe=5CD1F9B9"
-desc="Zaproś do siebie pogotowie kolędnicze! Wystarczy wypełnić <a href='https://goo.gl/forms/GJ5ZIPWjfG8B7OCz1'>formularz</a>, a przyjedziemy do Ciebie grać i śpiewać kolędy.">
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50016445_2366198670120916_4046718139046559744_n.jpg?_nc_cat=101&_nc_ht=scontent-frt3-1.xx&oh=0ba0e262c788b8cf18bf6fdea72dd61a&oe=5CBCA78C"
+desc="Potańcówka + wprawki taneczne w klimacie przedwojennej Łodzi. Poznacie polską odmianę tanga, fokstrota i walczyka.">
 </div>
 </div>
 
