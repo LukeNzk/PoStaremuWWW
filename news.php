716524b3343df2e117de29d9831080fcbd6cee0c
elejdor <elejdor@gmail.com>
2019-01-31 23:39:40 +0100

    Łódzkie Spotkania Mazurkowe - wszystkie daty

--- news.php
+++ news.php
@@ -16,11 +16,35 @@ desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjny
 </div>
 
 <div class="tile" 
+day="14" month="II" time="18:00" venue="Łódzki Dom Kultury"
+title="Łódzkie Spotkania Mazurkowe" 
+url="https://www.facebook.com/events/630671224032274/" 
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
+desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
+</div>
+  
+<div class="tile" 
 day="15" month="II" time="21:00" venue="Łódź"
 title="Orkiestra Taneczna Bonanza" 
 url="https://www.facebook.com/events/2165300870398818/" 
 img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50016445_2366198670120916_4046718139046559744_n.jpg?_nc_cat=101&_nc_ht=scontent-frt3-1.xx&oh=0ba0e262c788b8cf18bf6fdea72dd61a&oe=5CBCA78C"
 desc="Potańcówka + wprawki taneczne w klimacie przedwojennej Łodzi. Poznacie polską odmianę tanga, fokstrota i walczyka.">
+</div>
+
+<div class="tile" 
+day="19" month="II" time="18:00" venue="Łódzki Dom Kultury"
+title="Łódzkie Spotkania Mazurkowe" 
+url="https://www.facebook.com/events/630671224032274/" 
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
+desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
+</div>
+
+<div class="tile" 
+day="21" month="II" time="18:00" venue="Łódzki Dom Kultury"
+title="Łódzkie Spotkania Mazurkowe" 
+url="https://www.facebook.com/events/630671224032274/" 
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
+desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
 </div>
 </div>
 
