d0cefa11c82463cccab1215fa339f830e2400719
elejdor <elejdor@gmail.com>
2019-02-13 17:21:03 +0100

    Update news.php

--- news.php
+++ news.php
@@ -1,7 +1,49 @@
+<div id="tiles">
+<div class="tile" 
+day="3" month="II" time="16:30" venue="Poczekalnia / Więckowskiego"
+title="Karnawałowy Potańc Po Staremu // Kożuch" 
+url="https://www.facebook.com/events/221866625391755/" 
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/49773442_2354821757925274_6046371520979140608_o.jpg?_nc_cat=100&_nc_ht=scontent-frt3-1.xx&oh=4ef0397934dbedd9356b0108e2a69417&oe=5CFC86B9"
+desc="Impreza folkowa z muzykami znakomitej kapeli Kożuch. Przed imprezą warsztaty podstaw tańca oraz gry na bębenku obręczowym.">
+</div>
+
+<div class="tile" 
+day="12" month="II" time="18:00" venue="Łódzki Dom Kultury"
+title="Łódzkie Spotkania Mazurkowe" 
+url="https://www.facebook.com/events/630671224032274/" 
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
+desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
+</div>
+
 <div class="tile" 
 day="14" month="II" time="18:00" venue="Łódzki Dom Kultury"
 title="Łódzkie Spotkania Mazurkowe" 
 url="https://www.facebook.com/events/630671224032274/" 
 img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
 desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
+</div>
+  
+<div class="tile" 
+day="15" month="II" time="21:00" venue="Łódź"
+title="Orkiestra Taneczna Bonanza" 
+url="https://www.facebook.com/events/2165300870398818/" 
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50016445_2366198670120916_4046718139046559744_n.jpg?_nc_cat=101&_nc_ht=scontent-frt3-1.xx&oh=0ba0e262c788b8cf18bf6fdea72dd61a&oe=5CBCA78C"
+desc="Potańcówka + wprawki taneczne w klimacie przedwojennej Łodzi. Poznacie polską odmianę tanga, fokstrota i walczyka.">
+</div>
+
+<div class="tile" 
+day="19" month="II" time="18:00" venue="Łódzki Dom Kultury"
+title="Łódzkie Spotkania Mazurkowe" 
+url="https://www.facebook.com/events/630671224032274/" 
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
+desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
+</div>
+
+<div class="tile" 
+day="21" month="II" time="18:00" venue="Łódzki Dom Kultury"
+title="Łódzkie Spotkania Mazurkowe" 
+url="https://www.facebook.com/events/630671224032274/" 
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
+desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
+</div>
 </div>
