cfcfc79e6084e0de8f20d771fdddc83255487b32
elejdor <elejdor@gmail.com>
2019-02-13 17:19:51 +0100

    Update news.php

--- news.php
+++ news.php
@@ -5,19 +5,3 @@ url="https://www.facebook.com/events/630671224032274/"
 img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
 desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
 </div>
-
-<div class="tile" 
-day="19" month="II" time="18:00" venue="Łódzki Dom Kultury"
-title="Łódzkie Spotkania Mazurkowe" 
-url="https://www.facebook.com/events/630671224032274/" 
-img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
-desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
-</div>
-
-<div class="tile" 
-day="21" month="II" time="18:00" venue="Łódzki Dom Kultury"
-title="Łódzkie Spotkania Mazurkowe" 
-url="https://www.facebook.com/events/630671224032274/" 
-img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
-desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
-</div>
