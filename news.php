7eb4fc6b4efd6e4776f7711d7b3b9ae03b25aa31
elejdor <elejdor@gmail.com>
2019-02-13 17:19:25 +0100

    Update news.php

--- news.php
+++ news.php
@@ -5,14 +5,6 @@ url="https://www.facebook.com/events/630671224032274/"
 img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
 desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
 </div>
-  
-<div class="tile" 
-day="17" month="II" time="21:00" venue="Łódź, TU"
-title="Orkiestra Taneczna Bonanza" 
-url="https://www.facebook.com/events/242313783353740/" 
-img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50016445_2366198670120916_4046718139046559744_n.jpg?_nc_cat=101&_nc_ht=scontent-frt3-1.xx&oh=0ba0e262c788b8cf18bf6fdea72dd61a&oe=5CBCA78C"
-desc="Potańcówka + wprawki taneczne w klimacie przedwojennej Łodzi. Poznacie polską odmianę tanga, fokstrota i walczyka.">
-</div>
 
 <div class="tile" 
 day="19" month="II" time="18:00" venue="Łódzki Dom Kultury"
@@ -28,6 +20,4 @@ title="Łódzkie Spotkania Mazurkowe"
 url="https://www.facebook.com/events/630671224032274/" 
 img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
 desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
-</div>
-
 </div>
