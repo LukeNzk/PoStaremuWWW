db8ce416a387063d4b4a9558e88ef9dbd9e000a0
elejdor <elejdor@gmail.com>
2019-02-13 17:21:40 +0100

    Update news.php

--- news.php
+++ news.php
@@ -1,21 +1,5 @@
 <div id="tiles">
 <div class="tile" 
-day="3" month="II" time="16:30" venue="Poczekalnia / Więckowskiego"
-title="Karnawałowy Potańc Po Staremu // Kożuch" 
-url="https://www.facebook.com/events/221866625391755/" 
-img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/49773442_2354821757925274_6046371520979140608_o.jpg?_nc_cat=100&_nc_ht=scontent-frt3-1.xx&oh=4ef0397934dbedd9356b0108e2a69417&oe=5CFC86B9"
-desc="Impreza folkowa z muzykami znakomitej kapeli Kożuch. Przed imprezą warsztaty podstaw tańca oraz gry na bębenku obręczowym.">
-</div>
-
-<div class="tile" 
-day="12" month="II" time="18:00" venue="Łódzki Dom Kultury"
-title="Łódzkie Spotkania Mazurkowe" 
-url="https://www.facebook.com/events/630671224032274/" 
-img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
-desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
-</div>
-
-<div class="tile" 
 day="14" month="II" time="18:00" venue="Łódzki Dom Kultury"
 title="Łódzkie Spotkania Mazurkowe" 
 url="https://www.facebook.com/events/630671224032274/" 
@@ -24,9 +8,9 @@ desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjny
 </div>
   
 <div class="tile" 
-day="15" month="II" time="21:00" venue="Łódź"
+day="17" month="II" time="21:00" venue="Łódź, TU"
 title="Orkiestra Taneczna Bonanza" 
-url="https://www.facebook.com/events/2165300870398818/" 
+url="https://www.facebook.com/events/242313783353740/" 
 img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50016445_2366198670120916_4046718139046559744_n.jpg?_nc_cat=101&_nc_ht=scontent-frt3-1.xx&oh=0ba0e262c788b8cf18bf6fdea72dd61a&oe=5CBCA78C"
 desc="Potańcówka + wprawki taneczne w klimacie przedwojennej Łodzi. Poznacie polską odmianę tanga, fokstrota i walczyka.">
 </div>
@@ -45,5 +29,29 @@ title="Łódzkie Spotkania Mazurkowe"
 url="https://www.facebook.com/events/630671224032274/" 
 img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
 desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
+</div>
+
+<div class="tile" 
+day="25" month="II" time="18:30" venue="Łódzki Dom Kultury"
+title="Panie i Panowie, śpiewamy wiejskie ballady!" 
+url="https://www.facebook.com/events/560304524467813/" 
+img="https://scontent.fpoz1-1.fna.fbcdn.net/v/t1.0-9/51772156_2421760324564750_5533149497774833664_o.jpg?_nc_cat=111&_nc_ht=scontent.fpoz1-1.fna&oh=7daf688ea9c1d64a8a8fb8323dfcceeb&oe=5CED7FA9"
+desc="Koncert poświęcony grze legendarnego skrzypka Jana Bogusza z Kamiennej Woli położonej na pograniczu radomsko-opoczyńskim. Muzyka, opowieści, wiejskie legendy. Wstęp wolny.">
+</div>
+
+<div class="tile" 
+day="27" month="II" time="18:30" venue="Łódzki Dom Kultury"
+title="Skrzypce Bogusza // on/off kultura #10" 
+url="https://www.facebook.com/events/630671224032274/" 
+img="https://scontent.fpoz1-1.fna.fbcdn.net/v/t1.0-9/51783575_2421304717943644_738536398394490880_o.jpg?_nc_cat=111&_nc_ht=scontent.fpoz1-1.fna&oh=bd8ac896052b5484deec1d0e64791a67&oe=5CE51ED4"
+desc="Czy Jagusia dała w końcu Jankowi wianek? Z kim Mania nabyła dzieciąteczka? Jakimi liśćmi Janek przykrył Zosię? - wszystkich, chcących poznać odpowiedzi na te nurtujące pytania oraz pragnących uczyć się śpiewać zapraszamy na wspólne śpiewanie wiejskich ballad.">
+</div>
+
+<div class="tile" 
+day="3" month="III" time="12:00" venue="Poleski Ośrodek Sztuki"
+title="Raraszek" 
+url="https://www.facebook.com/events/1261170310716268/" 
+img="https://scontent.fpoz1-1.fna.fbcdn.net/v/t1.0-9/51559191_2406690252738424_5149481735670464512_o.jpg?_nc_cat=110&_nc_ht=scontent.fpoz1-1.fna&oh=6997b80d41d49b664585ca1639af3077&oe=5CF39D60"
+desc="Intensywne warsztaty krakowiaków i potańc przy muzyce z Małopolski na żywo. Zagrają doświadczeni kontynuatorzy i rekonstruktorzy muzyki tradycyjnej.">
 </div>
 </div>
