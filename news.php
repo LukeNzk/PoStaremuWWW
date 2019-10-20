af32f15be5c6c16f4c4ee347f989b0eeccc700c8
Luke N <LukeNzk@users.noreply.github.com>
2019-10-20 14:30:39 +0200

    Update news

--- news.php
+++ news.php
@@ -1,57 +1,25 @@
 <div id="tiles">
 <div class="tile" 
-day="14" month="II" time="18:00" venue="Łódzki Dom Kultury"
-title="Łódzkie Spotkania Mazurkowe" 
-url="https://www.facebook.com/events/630671224032274/" 
-img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
-desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
+day="20" month="X" time="16:00" venue="ZPiT Harnam"
+title="Potańc Po Staremu // łęczycko-łowicki" 
+url="https://www.facebook.com/events/1818723784937792/" 
+img="https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/72106185_3025970130810430_1193417827822862336_o.jpg?_nc_cat=111&_nc_oc=AQm_8kJiIg8TjIYCH0Oyt0gKBXZmtb7OVrcah39so2YLtXMDYbTCf_LiKlGT8zz3hFM&_nc_ht=scontent-frt3-2.xx&oh=925b12f14ecfd76217bfaff3be392936&oe=5E19890F"
+desc="Potańcówka z kapelami ludowymi. Mistrzowie tradycji zagrają w naturalnej, imprezowej sytuacji. Wstęp free.">
 </div>
   
 <div class="tile" 
-day="17" month="II" time="21:00" venue="Łódź, TU"
-title="Orkiestra Taneczna Bonanza" 
-url="https://www.facebook.com/events/242313783353740/" 
-img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50016445_2366198670120916_4046718139046559744_n.jpg?_nc_cat=101&_nc_ht=scontent-frt3-1.xx&oh=0ba0e262c788b8cf18bf6fdea72dd61a&oe=5CBCA78C"
-desc="Potańcówka + wprawki taneczne w klimacie przedwojennej Łodzi. Poznacie polską odmianę tanga, fokstrota i walczyka.">
+day="23" month="X" time="18:00" venue="Teatr Szwalnia"
+title="Środy Po Staremu" 
+url="https://www.facebook.com/pg/fundacja.postaremu/events/?ref=page_internal" 
+img="https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/53478289_2469678709772911_8312942448749838336_o.jpg?_nc_cat=105&_nc_oc=AQn4vOlzfxrf1jFW6KaT0BrifVyG92Q4jHRgZthn2Ha8kv4eV_CqsdZPwv87e-b3c5g&_nc_ht=scontent-frt3-2.xx&oh=2c3104924bfea3577be30c94957408c6&oe=5E5E2980"
+desc="Nauka śpiewu i tańca tradycyjnego od podstaw.">
 </div>
-
+  
 <div class="tile" 
-day="19" month="II" time="18:00" venue="Łódzki Dom Kultury"
-title="Łódzkie Spotkania Mazurkowe" 
-url="https://www.facebook.com/events/630671224032274/" 
-img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
-desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
-</div>
-
-<div class="tile" 
-day="21" month="II" time="18:00" venue="Łódzki Dom Kultury"
-title="Łódzkie Spotkania Mazurkowe" 
-url="https://www.facebook.com/events/630671224032274/" 
-img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/50229557_2366143660126417_8363287187810156544_o.jpg?_nc_cat=102&_nc_ht=scontent-frt3-1.xx&oh=41011f56ed15895d81feb67fa58adcc2&oe=5CF84C4A"
-desc="Zapraszamy instrumentalistów na każdym poziomie. Nauczymy was tradycyjnych melodii zebranych przez naszych muzyków oraz podstaw improwizacji. Spotkania 12, 14, 19, 21 lutego.">
-</div>
-
-<div class="tile" 
-day="25" month="II" time="18:30" venue="Łódzki Dom Kultury"
-title="Panie i Panowie, śpiewamy wiejskie ballady!" 
-url="https://www.facebook.com/events/560304524467813/" 
-img="https://scontent.fpoz1-1.fna.fbcdn.net/v/t1.0-9/51772156_2421760324564750_5533149497774833664_o.jpg?_nc_cat=111&_nc_ht=scontent.fpoz1-1.fna&oh=7daf688ea9c1d64a8a8fb8323dfcceeb&oe=5CED7FA9"
-desc="Koncert poświęcony grze legendarnego skrzypka Jana Bogusza z Kamiennej Woli położonej na pograniczu radomsko-opoczyńskim. Muzyka, opowieści, wiejskie legendy. Wstęp wolny.">
-</div>
-
-<div class="tile" 
-day="27" month="II" time="18:30" venue="Łódzki Dom Kultury"
-title="Skrzypce Bogusza // on/off kultura #10" 
-url="https://www.facebook.com/events/630671224032274/" 
-img="https://scontent.fpoz1-1.fna.fbcdn.net/v/t1.0-9/51783575_2421304717943644_738536398394490880_o.jpg?_nc_cat=111&_nc_ht=scontent.fpoz1-1.fna&oh=bd8ac896052b5484deec1d0e64791a67&oe=5CE51ED4"
-desc="Czy Jagusia dała w końcu Jankowi wianek? Z kim Mania nabyła dzieciąteczka? Jakimi liśćmi Janek przykrył Zosię? - wszystkich, chcących poznać odpowiedzi na te nurtujące pytania oraz pragnących uczyć się śpiewać zapraszamy na wspólne śpiewanie wiejskich ballad.">
-</div>
-
-<div class="tile" 
-day="3" month="III" time="12:00" venue="Poleski Ośrodek Sztuki"
-title="Raraszek" 
-url="https://www.facebook.com/events/1261170310716268/" 
-img="https://scontent.fpoz1-1.fna.fbcdn.net/v/t1.0-9/51559191_2406690252738424_5149481735670464512_o.jpg?_nc_cat=110&_nc_ht=scontent.fpoz1-1.fna&oh=6997b80d41d49b664585ca1639af3077&oe=5CF39D60"
-desc="Intensywne warsztaty krakowiaków i potańc przy muzyce z Małopolski na żywo. Zagrają doświadczeni kontynuatorzy i rekonstruktorzy muzyki tradycyjnej.">
+day="17" month="XI" time="18:00" venue=""
+title="Prusinowski Trio" 
+url="https://www.facebook.com/events/3328196780531565/" 
+img="https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/70961509_2984505631623547_3939099371143954432_n.jpg?_nc_cat=109&_nc_oc=AQlCXCsbd4qp7v2TVfyMBf1-IxENCfGw7GdgcJIzTCWfPZiv9B8KaoL6bC2ss24HtH4&_nc_ht=scontent-frt3-2.xx&oh=1ea6551b9019364adf11bd582e3dc06e&oe=5E22103A"
+desc="Impreza z jednymi z najlepszych muzyków tradycyjnych w Polsce.">
 </div>
 </div>
